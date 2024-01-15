6f2ead443f114fff70052906b911b1c53bf3a14d
ariefnugraha <arief.nugraha@graveltechnology.com>
2024-01-15 10:31:32 +0700

    qwdqdqw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Home = () => {
+  const router = useRouter();
   const [message, setMessage] = useState("");
   const [loding, setLoading] = useState("");
+
+  const handleClick = () => {
+    // Navigasi ke halaman lain
+    router.push("/home");
+  };
+
+  const handleClickLogin = () => {
+    const router = useRouter();
+
+    // Navigasi ke halaman lain
+    router.push("/login");
+  };
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent<any>) => {
@@ -23,23 +37,21 @@ const Home = () => {
     <div>
       <h1>Next.js App</h1>
       <h1>ini tokenmu {message}</h1>
-      <Link href={"home"}>
-        <button
-          style={{ background: "#1c1", cursor: "pointer", marginTop: 50 }}
-          className="cursor-pointer"
-        >
-          <h1>GO to mobile home</h1>
-        </button>
-      </Link>
+      <button
+        onClick={handleClick}
+        style={{ background: "#1c1", cursor: "pointer", marginTop: 50 }}
+        className="cursor-pointer"
+      >
+        <h1>GO to mobile home</h1>
+      </button>
 
-      <Link href={"login"}>
-        <button
-          style={{ background: "#dd1c33", cursor: "pointer", marginTop: 50 }}
-          className="cursor-pointer"
-        >
-          <h1>GO to Login</h1>
-        </button>
-      </Link>
+      <button
+        onClick={handleClickLogin}
+        style={{ background: "#dd1c33", cursor: "pointer", marginTop: 50 }}
+        className="cursor-pointer"
+      >
+        <h1>GO to Login</h1>
+      </button>
     </div>
   );
 };
